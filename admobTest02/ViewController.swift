172b2d41fad5ac54c6cac06f404f33791e580745
sabano3so2 <sabano3so2@gmail.com>
2020-10-12 23:37:38 +0900

    ストーリーボードで実装（10/12)

--- admobTest02/ViewController.swift
+++ admobTest02/ViewController.swift
@@ -10,54 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var bannerView:  GADBannerView!
     
     //var bannerView: GADBannerView! //追加した
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //
-//        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
 
-//        addBannerViewToView(bannerView)     //これ、使わない
-    
-//
-//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"  //テスト用ID
-//        bannerView.rootViewController = self
-//        bannerView.load(GADRequest())
-        
-//        super.viewDidLoad()
-        print("Google Mobile Ads SDK version: \(GADRequest.sdkVersion())")
+        print("Google Mobile Ads SDK version: \(GADMobileAds.sharedInstance().sdkVersion)")
         bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
         bannerView.rootViewController = self
         bannerView.load(GADRequest())
         
-        
-        
-        
       }
 
-//      func addBannerViewToView(_ bannerView: GADBannerView) {
-//        bannerView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(bannerView)
-//        view.addConstraints(
-//          [NSLayoutConstraint(item: bannerView,
-//                              attribute: .bottom,
-//                              relatedBy: .equal,
-//                              toItem: bottomLayoutGuide,
-//                              attribute: .top,
-//                              multiplier: 1,
-//                              constant: 0),
-//           NSLayoutConstraint(item: bannerView,
-//                              attribute: .centerX,
-//                              relatedBy: .equal,
-//                              toItem: view,
-//                              attribute: .centerX,
-//                              multiplier: 1,
-//                              constant: 0)
-//          ])
-//       }
-        // ここまで
+
 }
 
